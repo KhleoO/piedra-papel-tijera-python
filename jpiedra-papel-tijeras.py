e1e429d1ae31b3b7cd3b6bf1c74e29a812d05756
KhleoO <agustinavillanueva5a@gmail.com>
2024-11-12 20:37:34 -0800

    Update jpiedra-papel-tijeras.py
    Updated w/req tasks

--- jpiedra-papel-tijeras.py
+++ jpiedra-papel-tijeras.py
@@ -14,3 +14,35 @@ elif condicion1 or condicion2 or condicion3:
     print("Ha ganado:", nombre1)
 else:
     print("Ha ganado:", nombre2)
+
+#---------------------------------------------------------------------------------------------
+
+cant_intentos = 0
+cant_max_intentos = 3
+nombre1 = input("Cómo se llamará el Jugador1?: ")
+nombre2 = input("Cómo se llamará el Jugador2?: ")
+end_game = False
+
+while not end_game:
+    if not cant_intentos < cant_max_intentos:
+        print("Se han terminado las oportunidades")
+        break
+
+    print("Hola,", nombre1,"!")
+    jugador1 = input("Qué eliges? Piedra, papel o tijera?: ")
+    minjugador1 = jugador1.lower()
+    print("Hola,", nombre2,"!")
+    jugador2 = input("Qué eliges? Piedra, papel o tijera?: ")
+    minjugador2 = jugador2.lower()
+    
+    condicion1 = minjugador1 == "piedra" and minjugador2 == "tijera"
+    condicion2 = minjugador1 == "papel" and minjugador2 == "piedra"
+    condicion3 = minjugador1 == "tijera" and minjugador2 == "papel"
+    
+    if minjugador1 == minjugador2:
+        print("Ha sido un empate!")
+    elif condicion1 or condicion2 or condicion3:
+        print("Ha ganado:", nombre1)
+    else:
+        print("Ha ganado:", nombre2)
+    cant_intentos += 1
